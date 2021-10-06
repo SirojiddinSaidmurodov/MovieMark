12163f831ba504574a9e65e2830c22f169108f46
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2021-10-06 21:48:42 +0300

    Initial commit

---
+++ src/main/java/edu/keepeasy/moviemark/MovieMarkApplication.java
@@ -0,0 +1,13 @@
+package edu.keepeasy.moviemark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieMarkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieMarkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/keepeasy/moviemark/MovieMarkApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.keepeasy.moviemark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieMarkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
