eecf14e388662b77f2ea40b31ecea80e15a34f14
Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
2021-10-14 15:16:24 +0300

    delete password field from being serialized
    Took 4 minutes

--- src/main/java/edu/keepeasy/moviemark/model/User.java
+++ src/main/java/edu/keepeasy/moviemark/model/User.java
@@ -1,5 +1,7 @@
 package edu.keepeasy.moviemark.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.util.List;
 
@@ -11,6 +13,7 @@ public class User {
     @Column(name = "id", nullable = false)
     private Long id;
     private String username;
+    @JsonIgnore
     private String password;
     @Enumerated(EnumType.STRING)
     private Role role;
